0fa2df0e89701e703015abf880af8732261e1d4b
Gaia Cravesana <gcravesana@gmail.com>
2021-03-25 15:18:45 +0000

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Spot.new(name: "Alfama", description: "Typical neighborhood", rating: 3)
+Spot.new(name: "Principe real", description: "Typical neighborhood", rating: 4)
+Spot.new(name: "Mouraria", description: "Typical neighborhood", rating: 2)
+Spot.new(name: "Porta do sol", description: "Miradouro", rating: 5)
+Spot.new(name: "Miradouro do Graca", description: "Miradouro", rating: 3)
+Spot.new(name: "Santa Caterina", description: "Miradouro", rating: 4)
+Spot.new(name: "Praca do Comercio", description: "Main square", rating: 4)
