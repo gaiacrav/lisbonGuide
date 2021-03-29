8696a21bc0fb96ba5fb27946f10983e919f988a1
Gaia Cravesana <gcravesana@gmail.com>
2021-03-29 15:46:41 +0100

    added mapbox api key and in the model

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,5 +1,7 @@
 class Spot < ApplicationRecord
   has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   validates :name, presence: true
   #validates :description, presence: true
