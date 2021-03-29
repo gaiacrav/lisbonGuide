79dc5f8709a00b18308aca6f3cb84cd0ab3816af
Gaia Cravesana <gcravesana@gmail.com>
2021-03-29 17:48:18 +0100

    added slider

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+document.addEventListener('turbolinks:load', () => {
+  initMapbox();
+})
