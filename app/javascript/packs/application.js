8c2af0e2804c4b5dc04c72a7f0ce72c292c125d1
Gaia Cravesana <gcravesana@gmail.com>
2021-03-29 18:54:02 +0100

    remove map box

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,10 +16,3 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import 'mapbox-gl/dist/mapbox-gl.css';
-
-import { initMapbox } from '../plugins/init_mapbox';
-
-document.addEventListener('turbolinks:load', () => {
-  initMapbox();
-})
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,7 +1,5 @@
 class Spot < ApplicationRecord
   has_one_attached :photo
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
 
   validates :name, presence: true
   #validates :description, presence: true
