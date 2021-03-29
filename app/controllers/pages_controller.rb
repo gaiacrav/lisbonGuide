8de14b90389c321b8aca864fce002a8f9bf14e82
Gaia Cravesana <gcravesana@gmail.com>
2021-03-29 15:48:54 +0100

    working on maps

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,16 @@ class PagesController < ApplicationController
 
   def home
     @spots = Spot.all
+
+    @markers = @spots.geocoded.map do |spot|
+      {
+        lat: spot.latitude,
+        lng: spot.longitude
+      }
+    end
   end
+
+
 
 
 end
